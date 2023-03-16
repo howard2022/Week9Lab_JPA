9bb8672ec14a325a55fe14b288d6e1c2d881e4a0
howard2022 <howard.hyca@gmail.com>
2023-03-15 18:21:12 -0600

    Updated UserService working on servlets

--- src/java/dataaccess/RoleDB.java
+++ src/java/dataaccess/RoleDB.java
@@ -1,14 +1,20 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package dataaccess;
 
-/**
- *
- * @author howar
- */
+import javax.persistence.EntityManager;
+import models.Role;
+
 public class RoleDB {
-    
+
+    public Role get(int roleID) {
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+
+        try {
+            Role role = em.find(Role.class, roleID);
+
+            return role;
+        } finally {
+            em.close();
+        }
+
+    }
 }
--- src/java/services/UserService.java
+++ src/java/services/UserService.java
@@ -1,5 +1,6 @@
 package services;
 
+import dataaccess.RoleDB;
 import dataaccess.UserDB;
 import java.util.ArrayList;
 import java.util.List;
@@ -21,23 +22,30 @@ public class UserService {
     }
     
     public void insert(String email, String firstname, String lastname, String password, int roleID) throws Exception {
-        Role role = new Role(roleID);
-        User user = new User(email,firstname,lastname,password,role);
+        
+        User user = new User(email,firstname,lastname,password);
+        RoleDB roleDB = new RoleDB();
+        Role role = roleDB.get(roleID);
+        user.setRole(role);
+        
         UserDB userdb = new UserDB();
         userdb.insert(user);
     }
     
     public void update(String email, String firstname, String lastname, String password, int roleID) throws Exception {
-        Role role = new Role(roleID);
-        User user = new User(email,firstname,lastname,password,role);
         UserDB userdb = new UserDB();
+        RoleDB roleDB = new RoleDB();
+        Role role = roleDB.get(roleID);
+        User user = userdb.get(email);
+        user.setFirstName(firstname);
+        user.setLastName(lastname);
+        user.setRole(role);
         userdb.update(user);
     }
     
     public void delete(String email) throws Exception {
-        User user = new User();
-        user.setEmail(email);
         UserDB userdb = new UserDB();
+        User user = userdb.get(email);        
         userdb.delete(user);
     }
 
